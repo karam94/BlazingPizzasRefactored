eea73022e0aafc48c1ab68f547cf16adf369b8be
Karam Kabbara <contact@karam.io>
2020-11-11 18:10:43 +0000

    Extracted pizza card list out in to separate component

--- src/BlazingPizza.Client/Program.cs
+++ src/BlazingPizza.Client/Program.cs
@@ -15,7 +15,7 @@ namespace BlazingPizza.Client
             builder.RootComponents.Add<App>("app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-            builder.Services.AddScoped<OrderState>();
+            builder.Services.AddSingleton<OrderState>();
 
             // Add auth services
             builder.Services.AddApiAuthorization<PizzaAuthenticationState>(options =>
